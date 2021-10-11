85c9506667b780e9ea043452c073589417fe8026
shaho1090 <shaho.parvini@gmail.com>
2021-10-11 16:43:30 +0330

    add json respounce

--- app/Http/Controllers/MyNoteController.php
+++ app/Http/Controllers/MyNoteController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\NoteStoreRequest;
+use App\Http\Resources\NoteResource;
 use App\Models\Note;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Response;
 
 class MyNoteController extends Controller
 {
-    public function store(NoteStoreRequest $request)
+    public function store(NoteStoreRequest $request): JsonResponse
     {
         $request['author_id'] = auth()->id();
 
         $note = (new Note())->createNew($request->toArray());
 
+        return Response::json([
+            'data' => new NoteResource($note)
+        ]);
     }
 }
