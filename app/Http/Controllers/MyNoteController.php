34df3f68339d8ffbcd8bf181d8a42f0e5118a0bb
shaho1090 <shaho.parvini@gmail.com>
2021-10-11 15:20:39 +0330

    create controller for note

---
+++ app/Http/Controllers/MyNoteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyNoteController extends Controller
+{
+    public function store(NoteStoreRequest $request)
+    {
+
+    }
+}
--- routes/note.php
+++ routes/note.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MyNoteController;
 use Illuminate\Support\Facades\Route;
 
 
