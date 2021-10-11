4f8a9c54fcd4e7e6d746fe7aed5ccaad33f7a0e4
shaho1090 <shaho.parvini@gmail.com>
2021-10-11 16:45:25 +0330

    test progress for note

--- tests/Feature/NoteTest.php
+++ tests/Feature/NoteTest.php
@@ -2,36 +2,60 @@
 
 namespace Tests\Feature;
 
+use App\Models\Note;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class NoteTest extends TestCase
 {
-   use RefreshDatabase;
+    use RefreshDatabase;
 
-   public function test_a_user_can_create_a_note()
-   {
-       $user = User::factory()->create();
+    public function test_a_user_can_create_a_note()
+    {
+        $user = User::factory()->create();
 
-       Sanctum::actingAs($user);
+        Sanctum::actingAs($user);
 
-       $this->assertAuthenticated();
+        $this->assertAuthenticated();
 
-       $noteData = [
-           'title' => 'simple title',
-           'note' => 'this is simple note'
-       ];
+        $noteData = [
+            'title' => 'simple title',
+            'note' => 'this is simple note'
+        ];
 
-       $this->postJson(route('note.store'),$noteData)->dump();
+        $this->postJson(route('note.store'), $noteData)
+            ->assertStatus(200)
+            ->assertJsonFragment([
+                'title' => $noteData['title'],
+                'note' => $noteData['note']
+            ])->assertJsonFragment([
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email
+            ]);
 
-       $this->assertDatabaseHas('notes',[
-           'author_id' => $user->id,
-           'title' => $noteData['title'],
-           'note' => $noteData['note']
-       ]);
+        $this->assertDatabaseHas('notes', [
+            'author_id' => $user->id,
+            'title' => $noteData['title'],
+            'note' => $noteData['note']
+        ]);
 
-   }
+    }
+
+    public function test_a_user_can_update_a_title_of_a_note()
+    {
+        $user = User::factory()->hasNotes(3)->create();
+
+        Sanctum::actingAs($user);
+
+        $noteData =[
+            'title' => 'another title for the note'
+        ];
+
+        $this->patchJson(route('note.update',$user->first()->id),$noteData)->dump();
+
+        $this->assertSame( $user->first()->title, $noteData['title']);
+    }
 }
